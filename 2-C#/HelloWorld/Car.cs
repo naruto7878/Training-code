743b1d9a2a35c4f03f8c3e6f40fac842f56b0f77
Scifiler-sys <pagdilaostephen@gmail.com>
2022-01-19 15:03:55 -0500

    Added demos for today

--- 2-C#/HelloWorld/Car.cs
+++ 2-C#/HelloWorld/Car.cs
@@ -5,6 +5,67 @@ namespace CarFunction
     //Public is just there to make the class visible for everyone else
     public class Car
     {
+
+        //Field
+        //It is used to store information or define the current state of the object when you first make it
+        //private means that only the class itself we can use that field
+        private string _color;
+        private string _owner;
+        private int _gallonPerMile;
+
+        //Property
+        //They are in PascalCase ThisIsHowPascalCaseWillMakeASentence
+        public string Color
+        {
+            //get keyword in a property is how you can make one of your private fields readable
+            get { return "The color is "+_color; }
+
+            //set keyword in a property is how you can make one of your private fields writable
+            set { _color = value; }
+        }
+        
+        public string Owner
+        {
+            get { return _owner; }
+            set { _owner = value; }
+        }
+
+        public int Fuel { get; set; }
+
+        //Constructor
+        //It is a special method that will run first whenever you create an object out of that class
+        public Car()
+        {
+            _color = "Blue";
+            _gallonPerMile = 10;
+            _owner = "No Owner";
+            Fuel = 100;
+        }
+        
+        //A method will run multiple lines of code to do some sort of operation/behavior/function
+        //void this method will return/give back nothing
+        //We can change void into another datatype if you want the method to give information back
+        public void Start()
+        {
+            Console.WriteLine("The car is starting right now!");
+            Console.WriteLine($"Current fuel: {Fuel}");
+        }
+
+        //You can add parameters to a method to pass in data to be used inside method
+        //Make sure the parameter has a datatype and then a name
+        public void Start(int p_fuel)
+        {
+            Fuel = p_fuel;
+            Console.WriteLine("The car is starting right now!");
+            Console.WriteLine($"Current fuel: {Fuel}");
+        }
+
+        //Will give the total distance of a car
+        //Methods can return data back if you didn't put void
+        public double TotalDistance()
+        {
+            return (double)Fuel/_gallonPerMile;
+        }
         
     }
 }
--- 2-C#/HelloWorld/Program.cs
+++ 2-C#/HelloWorld/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CarFunction;
+
+//This will print whatever string you put in the parenthesis into the terminal
+// Console.WriteLine("Hello, World!");
+
+// //NameOfClass NameOfVariable = new NameOfClass();
+// //Initializing an object out of a class
+// Car car1 = new Car();
+
+// Console.WriteLine(car1.Color);
+// car1.Color = "Red";
+// Console.WriteLine(car1.Color);
+// car1.Fuel = 10;
+// Console.WriteLine(car1.Fuel);
+// car1.Start();
+// car1.Start(100);
+
+// //Another way of initializing objects out of a class but preconfiguring our states already
+// Car car2 = new Car()
+// {
+//     Color = "Yellow",
+//     Fuel = 12,
+//     Owner = "Stephen"
+// };
+
+// Console.WriteLine(car2.Color);
+// Console.WriteLine(car2.TotalDistance());
+bool repeat = true;
+
+while (repeat)
+{
+    Console.Clear();
+    Console.WriteLine("Welcome to programming!");
+    Console.WriteLine("Please tell me your name");
+    //Console.ReadLine() method will always return a string
+    string name = Console.ReadLine();
+    Console.WriteLine($"Hello {name}, what do you want to do today?");
+    Console.WriteLine("[1] - Add two numbers");
+    Console.WriteLine("[2] - Exit");
+
+    string userInput = Console.ReadLine();
+
+    if (userInput == "1")
+    {
+        Console.WriteLine("Please give me two numbers");
+        //Convert class is designed for us to convert datatypes into another datatype
+        //It isn't perfect
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"The sum is: {num2+num1}");
+        Console.WriteLine("Please press Enter to continue");
+        Console.ReadLine();
+    }
+    else if(userInput == "2")
+    {
+        repeat = false;
+    }
+
+}
