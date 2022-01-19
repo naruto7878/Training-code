54920bd62dc4a24eaea5eff498541cbd2c075293
Scifiler-sys <pagdilaostephen@gmail.com>
2022-01-19 12:33:25 -0500

    First init

---
+++ 2-C#/HelloWorld/Car.cs
@@ -0,0 +1,10 @@
+//Namespace is a great way to organize your code
+namespace CarFunction
+{
+    //This is how you create a class by using the class keyword
+    //Public is just there to make the class visible for everyone else
+    public class Car
+    {
+        
+    }
+}
