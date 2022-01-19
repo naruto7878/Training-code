671387494ab979e1d2af6e8985baf8d16e797ffe
Scifiler-sys <pagdilaostephen@gmail.com>
2022-01-19 17:21:37 -0500

    Added collection demo

---
+++ 2-C#/HelloWorld/Collection.cs
@@ -0,0 +1,96 @@
+namespace CollectionFunction
+{
+    public class Collection
+    {
+        //Array
+        //Used to store a datatype and have a fixed size
+        //zero-based index
+        //Syntax: (datatype)[] (name of variable) = new (datatype)[(size of the array)];
+        private int[] _nums = new int[5];
+
+        //Generic Collection
+        //They store a specific datatype and have variable size
+        //Syntax: <(datatype)>
+
+        //List Collection
+        //zero-based index
+        //Used to store a datatype and has variable size
+        private List<string> _strings = new List<string>();
+
+        //Hashset Collection
+        //There is no particular order to the elements so NOT zero-based index
+        //You cannot have duplicate elements 
+        private HashSet<int> _numsCollection = new HashSet<int>();
+
+        //Dictionary Collection
+        //Stores information through key-value pairs
+        //There is no particular order 
+        private Dictionary<string,int> _directory = new Dictionary<string, int>();
+
+        //Non-generic Collection
+        //They store any datatype and have variable size
+
+        public void CollectionMain()
+        {
+            Console.WriteLine("===Collection Demo===");
+
+            _nums[0] = 3;
+            _nums[1] = 10;
+            _nums[2] = 4;
+
+            //A way to go through a list
+            //Foreach will iterate through all of the elements of a collection/array
+            //Syntax: ( (datatype) (Name of variable) in (Name of array/collection) )
+            Console.WriteLine("===Foreach Loop===");
+            foreach (int num in _nums)
+            {
+                Console.WriteLine(num);
+            }
+
+            //For loop is for a more complex way to iterate through a collection
+            //Syntax: ((block 1 used to initialize some temp variable); (block 2 - used for some condition to satisfy); (block 3 - how you want to change your temp variable))
+            Console.WriteLine("===For Loop===");
+            for (int i = 0; i < _nums.Length; i+=2)
+            {
+                Console.WriteLine("Current value of i: "+ i);
+                Console.WriteLine(_nums[i]);
+            }
+
+            Console.WriteLine("===Generic Collection===");
+            Console.WriteLine("=List Demo=");
+            _strings.Add("First element");
+            _strings.Add("Second element");
+            _strings.Add("Third element");
+            _strings.Add("First element");
+
+            foreach (string item in _strings)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(_strings[1]);
+
+            Console.WriteLine("=Hashset Demo=");
+            _numsCollection.Add(1);
+            _numsCollection.Add(2);
+            _numsCollection.Add(3);
+            _numsCollection.Add(1);
+
+            foreach (int item in _numsCollection)
+            {
+                Console.WriteLine(item);
+            }
+            // Console.WriteLine(numsCollection[1]); --Gives an exception due to hashset not being zero-based index
+
+            Console.WriteLine("=Dictionary Demo=");
+            _directory.Add("Stephen", -10000);
+            _directory.Add("Vijhan", 1000);
+            _directory.Add("Jonathon", -100000);
+            // _directory.Add("Stephen", 1000); --Will give an exception due to Stephen key already existing
+
+            //Dictionary uses key to look up information from its stored data
+            Console.WriteLine(_directory["Stephen"]);
+            Console.WriteLine(_directory["Jonathon"]);
+            
+        }
+    }
+}
--- 2-C#/HelloWorld/Program.cs
+++ 2-C#/HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CarFunction;
+using CollectionFunction;
 
 //This will print whatever string you put in the parenthesis into the terminal
 // Console.WriteLine("Hello, World!");
@@ -26,37 +27,41 @@ using CarFunction;
 
 // Console.WriteLine(car2.Color);
 // Console.WriteLine(car2.TotalDistance());
-bool repeat = true;
+// bool repeat = true;
 
-while (repeat)
-{
-    Console.Clear();
-    Console.WriteLine("Welcome to programming!");
-    Console.WriteLine("Please tell me your name");
-    //Console.ReadLine() method will always return a string
-    string name = Console.ReadLine();
-    Console.WriteLine($"Hello {name}, what do you want to do today?");
-    Console.WriteLine("[1] - Add two numbers");
-    Console.WriteLine("[2] - Exit");
+// while (repeat)
+// {
+//     Console.Clear();
+//     Console.WriteLine("Welcome to programming!");
+//     Console.WriteLine("Please tell me your name");
+//     //Console.ReadLine() method will always return a string
+//     string name = Console.ReadLine();
+//     Console.WriteLine($"Hello {name}, what do you want to do today?");
+//     Console.WriteLine("[1] - Add two numbers");
+//     Console.WriteLine("[2] - Exit");
 
-    string userInput = Console.ReadLine();
+//     string userInput = Console.ReadLine();
 
-    if (userInput == "1")
-    {
-        Console.WriteLine("Please give me two numbers");
-        //Convert class is designed for us to convert datatypes into another datatype
-        //It isn't perfect
-        int num1 = Convert.ToInt32(Console.ReadLine());
+//     if (userInput == "1")
+//     {
+//         Console.WriteLine("Please give me two numbers");
+//         //Convert class is designed for us to convert datatypes into another datatype
+//         //It isn't perfect
+//         int num1 = Convert.ToInt32(Console.ReadLine());
 
-        int num2 = Convert.ToInt32(Console.ReadLine());
+//         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine($"The sum is: {num2+num1}");
-        Console.WriteLine("Please press Enter to continue");
-        Console.ReadLine();
-    }
-    else if(userInput == "2")
-    {
-        repeat = false;
-    }
+//         Console.WriteLine($"The sum is: {num2+num1}");
+//         Console.WriteLine("Please press Enter to continue");
+//         Console.ReadLine();
+//     }
+//     else if(userInput == "2")
+//     {
+//         repeat = false;
+//     }
 
-}
+// }
+
+//Initialized Collection class
+Collection collect = new Collection();
+collect.CollectionMain();
